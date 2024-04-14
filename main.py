e7bf698b881b9e6720a652d8f8f73623ce49b8c3
McPolygon <remove>
2024-04-14 11:06:38 -0500

    1.0.0

--- main.py
+++ main.py
@@ -1,49 +1,17 @@
-import os
-
-import pandas as pd
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
-
-from ml.data import apply_label, process_data
-from ml.model import inference, load_model
-
-# DO NOT MODIFY
-class Data(BaseModel):
-    age: int = Field(..., example=37)
-    workclass: str = Field(..., example="Private")
-    fnlgt: int = Field(..., example=178356)
-    education: str = Field(..., example="HS-grad")
-    education_num: int = Field(..., example=10, alias="education-num")
-    marital_status: str = Field(
-        ..., example="Married-civ-spouse", alias="marital-status"
-    )
-    occupation: str = Field(..., example="Prof-specialty")
-    relationship: str = Field(..., example="Husband")
-    race: str = Field(..., example="White")
-    sex: str = Field(..., example="Male")
-    capital_gain: int = Field(..., example=0, alias="capital-gain")
-    capital_loss: int = Field(..., example=0, alias="capital-loss")
-    hours_per_week: int = Field(..., example=40, alias="hours-per-week")
-    native_country: str = Field(..., example="United-States", alias="native-country")
-
-path = # TODO: enter the path for the saved encoder 
+path =  "model/encoder.pkl"
 encoder = load_model(path)
 
-path = # TODO: enter the path for the saved model 
+path = "model/model.pkl"
 model = load_model(path)
 
-# TODO: create a RESTful API using FastAPI
-app = # your code here
+app = FastAPI() 
 
-# TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
     # your code here
-    pass
+    return {"greeting": "Hello World!"}
 
-
-# TODO: create a POST on a different path that does model inference
 @app.post("/data/")
 async def post_inference(data: Data):
     # DO NOT MODIFY: turn the Pydantic model into a dict.
@@ -68,7 +36,12 @@ async def post_inference(data: Data):
         # your code here
         # use data as data input
         # use training = False
-        # do not need to pass lb as input
-    )
-    _inference = # your code here to predict the result using data_processed
+        # do not need to pass lb as input    
+        data,     
+        cat_features,
+        training=False,
+        encoder=encoder,
+    )        
+        
+    _inference = inference(model, data_processed)  # your code here to predict the result using data_processed
     return {"result": apply_label(_inference)}
