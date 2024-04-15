0ff7a26cc248cbfba63d33792d405ef159254d6f
McPolygon <17949585+McPolygon@users.noreply.github.com>
2024-04-15 11:13:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+import os
+import pandas as pd
+from fastapi import FastAPI
+from ml.data import apply_label, process_data
+from ml.model import inference, load_model
+from pydantic import BaseModel, Field
+
+#define Data class and use base model
+class Data(BaseModel):
+    age: int = Field(..., example=37)
+    workclass: str = Field(..., example="Private")
+    fnlgt: int = Field(..., example=178356)
+    education: str = Field(..., example="HS-grad")
+    education_num: int = Field(..., example=10, alias="education-num")
+    marital_status: str = Field(
+        ..., example="Married-civ-spouse", alias="marital-status"
+    )
+    occupation: str = Field(..., example="Prof-specialty")
+    relationship: str = Field(..., example="Husband")
+    race: str = Field(..., example="White")
+    sex: str = Field(..., example="Male")
+    capital_gain: int = Field(..., example=0, alias="capital-gain")
+    capital_loss: int = Field(..., example=0, alias="capital-loss")
+    hours_per_week: int = Field(..., example=40, alias="hours-per-week")
+    native_country: str = Field(..., example="United-States", alias="native-country")
+    
 path =  "model/encoder.pkl"
 encoder = load_model(path)
 
